2368a7bc6d4d4e4cec341d9ceca45407bba5b4bc
4xDMG <dajtait87@gmail.com>
2017-05-08 14:24:16 +1000

    Changed Header and removed React logo.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import GameMap from './components/gameMap';
 
@@ -19,8 +18,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Scissor Warrior</h2>
         </div>
         <div>
           <GameMap
